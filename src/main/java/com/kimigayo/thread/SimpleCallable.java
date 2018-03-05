47026736718c244dcc8fc402596c0ea14f22b461
chenbinbin <1056477503@qq.com>
2018-03-06 00:12:30 +0800

    线程同步synchronized

--- src/main/java/com/kimigayo/thread/SimpleCallable.java
+++ src/main/java/com/kimigayo/thread/SimpleCallable.java
@@ -3,12 +3,21 @@ package com.kimigayo.thread;
 import java.util.concurrent.Callable;
 
 public class SimpleCallable implements Callable {
-    private int num =100;
+    private int num = 1000;
+
     public Object call() throws Exception {
-        int i=0;
-        for(;num>0;i++){
-            System.out.println(Thread.currentThread().getName()+"==>"+num--);
+        int i = 0;
+        while(true) {
+            synchronized (this) {
+                if (num > 0) {
+                    i++;
+                    System.out.println(Thread.currentThread().getName() + "==>" + num--);
+                }else {
+                    break;
+                }
+            }
         }
-        return Thread.currentThread().getName()+"="+i;
+        Thread.sleep(2000);
+        return Thread.currentThread().getName() + "=" + i;
     }
 }
--- src/main/java/com/kimigayo/thread/SimpleTest.java
+++ src/main/java/com/kimigayo/thread/SimpleTest.java
@@ -3,7 +3,7 @@ package com.kimigayo.thread;
 import java.util.concurrent.*;
 
 public class SimpleTest {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 //        SimpleThread thread = new SimpleThread();
 //        Thread thread1 = new Thread(new SimpleRunnable());
 //        FutureTask task = new FutureTask(new SimpleCallable());
@@ -24,45 +24,45 @@ public class SimpleTest {
 //        thread3.start();
 
         //Callable变量同步测试,存在同步问题
-//        SimpleCallable callable = new SimpleCallable();
-//        FutureTask task1 = new FutureTask(callable);
-//        FutureTask task2 = new FutureTask(callable);
-//        FutureTask task3 = new FutureTask(callable);
-//        Thread thread1 = new Thread(task1);
-//        Thread thread2 = new Thread(task2);
-//        Thread thread3 = new Thread(task3);
-//        thread1.start();
-//        thread2.start();
-//        thread3.start();
-//        System.out.println(task1.get());
-//        System.out.println(task2.get());
-//        System.out.println(task3.get());
+        SimpleCallable callable = new SimpleCallable();
+        FutureTask task1 = new FutureTask(callable);
+        FutureTask task2 = new FutureTask(callable);
+        FutureTask task3 = new FutureTask(callable);
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+        Thread thread3 = new Thread(task3);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        System.out.println(task1.get());
+        System.out.println(task2.get());
+        System.out.println(task3.get());
 
-        SimpleCallable callable1 = new SimpleCallable();
-        SimpleCallable callable2 = new SimpleCallable();
-        ExecutorService service =  Executors.newFixedThreadPool(2);
-        Future future1 = service.submit(callable1);
-        Future future2 = service.submit(callable2);
-        try {
-            System.out.println("future1执行结果="+future1.get(1,TimeUnit.MICROSECONDS));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-            System.out.println("future1执行超时");
-        }
-        try {
-            System.out.println("future2执行结果="+future2.get(1,TimeUnit.MICROSECONDS));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-            System.out.println("future2执行超时");
-        }
-        service.shutdownNow();
+//        SimpleCallable callable1 = new SimpleCallable();
+//        SimpleCallable callable2 = new SimpleCallable();
+//        ExecutorService service =  Executors.newFixedThreadPool(2);
+//        Future future1 = service.submit(callable1);
+//        Future future2 = service.submit(callable2);
+//        try {
+//            System.out.println("future1执行结果="+future1.get(1,TimeUnit.MICROSECONDS));
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (TimeoutException e) {
+//            e.printStackTrace();
+//            System.out.println("future1执行超时");
+//        }
+//        try {
+//            System.out.println("future2执行结果="+future2.get(1,TimeUnit.MICROSECONDS));
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (TimeoutException e) {
+//            e.printStackTrace();
+//            System.out.println("future2执行超时");
+//        }
+//        service.shutdownNow();
    }
 }
